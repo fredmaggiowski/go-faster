0d434e1f0f0518b13092b4f98ee46d05d8df91ca
Federico Maggi <federico.maggi@xtn-lab.com>
2019-09-05 00:21:47 +0200

    fixed chan length for same-routine accesses

--- hub.go
+++ hub.go
@@ -27,12 +27,12 @@ type Hub struct {
 
 func newHub() *Hub {
 	return &Hub{
-		broadcast:  make(chan []byte),
+		clients:    make(map[*Client]bool),
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
-		clients:    make(map[*Client]bool),
-		turn:       make(chan *Client),
-		playedTurn: make(chan *Client),
+		broadcast:  make(chan []byte, 1),
+		turn:       make(chan *Client, 1),
+		playedTurn: make(chan *Client, 1),
 	}
 }
 
@@ -40,26 +40,35 @@ func (h *Hub) startGame() error {
 	if len(h.clients) == 0 {
 		return fmt.Errorf("no player")
 	}
+
 	// Reset all turns
 	for client := range h.clients {
 		h.clients[client] = false
 	}
-	h.selectTurn() // Start first turn
+
+	// Start first turn
+	if !h.selectTurn() {
+		return fmt.Errorf("game over")
+	}
 
 	return nil
 }
 
-func (h *Hub) selectTurn() {
+func (h *Hub) selectTurn() bool {
 	notYetPlayed := make([]*Client, 0)
 	for client, played := range h.clients {
 		if !played && client.name != "admin" {
 			notYetPlayed = append(notYetPlayed, client)
 		}
 	}
+	if len(notYetPlayed) == 0 {
+		return false
+	}
 
 	turn := rand.Intn(len(notYetPlayed))
 	h.turn <- notYetPlayed[turn]
 	h.clients[notYetPlayed[turn]] = true
+	return true
 }
 
 func (h *Hub) run() {
@@ -83,11 +92,14 @@ func (h *Hub) run() {
 
 		case client := <-h.playedTurn:
 			h.clients[client] = true
-			h.selectTurn()
+			if !h.selectTurn() {
+				log.Println("No more players, game is over")
+				h.broadcast <- []byte("game:over")
+			}
 
 		case message := <-h.broadcast:
 			for client := range h.clients {
-				log.Println("Sending to client:", client.name)
+				log.Printf("Broadcasting message to client %s.\n", client.name)
 				select {
 				case client.send <- message:
 				default:
